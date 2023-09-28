1c345ba2ea621719e4deb19ff34eaed021ff6b0b
Hasan <mohammed.6.ahmed@kcl.ac.uk>
2023-09-28 22:58:28 +0100

    Add Changes to frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,9 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    items = [str(item) for item in items]
+
+    for value in items:
+        frequencies[value] = frequencies.get(value , 0) + 1
+    
     return frequencies
