ad72c39cc490ad9a91569ddd20a0f1b84c48939f
Hasan <mohammed.6.ahmed@kcl.ac.uk>
2023-09-28 23:06:37 +0100

    Add comments to previous(1c345ba) commit

--- frequencies.py
+++ frequencies.py
@@ -4,8 +4,11 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+
+    # Converts all values in the list to a string
     items = [str(item) for item in items]
 
+    # goes through each item of the list, adds an index if it doesnt exist - starting from 1
     for value in items:
         frequencies[value] = frequencies.get(value , 0) + 1
     
